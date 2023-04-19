8f5a70e5fcbf425f863fd5889b888c58ad394810
Kader NUR TEKİN <tekinkadernur@gmail.com>
2023-04-19 23:23:37 +0300

    pratik yıldız

---
+++ yildiz ucgen/src/yildizucgen.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class yildizucgen {
+
+	public static void main(String[] args) {
+		// Java'da döngüler kullanılarak yıldızlar ile üçgen yapıyoruz.
+		
+		
+		Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        
+        
+        System.out.println("---------------------------");
+        
+       
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >=0  ; i--) {
+            for (int j = (n - i); j >0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >=1 ; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }    
+            
+        
+        
+        
+       
+
+        
+
+
+	}
+
+}
